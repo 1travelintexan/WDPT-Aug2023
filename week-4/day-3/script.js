d0131cd6c8788fb9ad7655a0e4a21ffb49f79dbb
Joshua <horheyinc8@gmail.com>
2023-09-09 12:47:38 +0200

    week 4 day 3

--- week-4/day-3/script.js
+++ week-4/day-3/script.js
@@ -1,60 +1,93 @@
 console.log("connected");
+//example of a global variable
+let globalVariable = "this is a global variable";
+//inside if statements or functions, this variable is accessible
 
-// try / catch
-try {
-  const username = prompt("enter you name");
-  if (username.length < 3) {
-    throw new Error("Your name isnt long enough");
-  } else {
-    alert("Hooray! Great job");
-  }
-} catch (err) {
-  alert(err);
-  console.log("there was an error", err);
+//example of a block level variable
+{
+  const blockVariable = "this is a block variable";
 }
 
-const labEx = [
-  { name: "my cool movie", rating: 9.5 },
-  { name: "my not cool movie", rating: 5 },
-  { name: "my movie", rating: 5 },
-];
-//.sort()
-//takes one callback function
-const numbers = [2, 1, 70, 70, 4, 15];
-const sortedAsc = numbers.sort((a, b) => {
-  if (a > b) {
-    return 3;
-  } else if (a < b) {
-    return -3;
-  } else {
-    return 0;
+//functional scope
+function testingScopes() {
+  var testFuncVariable = "Function variable";
+}
+//this is just testing our scopes
+if (true) {
+  console.log("here is our global,", globalVariable);
+  console.log("var declared after", iansquestion);
+  //   console.log("const declared after", testForConst);
+  //   console.log("function variable", testFuncVariable);
+  //   console.log("block, ", blockVariable);
+}
+
+//var declared before console.logging
+var iansquestion = "var globally scoped";
+const testForConst = "this a const declared after";
+var notConstExample = "This is weird";
+console.log("will it work", notConstExample);
+console.log(window);
+
+//<==================Hoisting ======================>
+//function being hoisted
+const sum = add(2, 3);
+
+function add(a, b) {
+  console.log("add function", a + b);
+  return a + b;
+}
+
+console.log("this is after the function is run", sum);
+
+//<=============variable shadowing =============>
+let name = "Ruben";
+if (true) {
+  const name = "Dinah";
+  console.log("inside the if", name);
+}
+console.log("outside the if", name);
+
+//<===============value vs Ref ================>
+console.log("ref ex", "Chloe" === "Chloe");
+console.log(2 === 2);
+console.log(2 == "2");
+
+console.log([1, 2, 3] === [1, 2, 3]);
+//reference of an array
+const array1 = [1, 2, 3];
+//not recommended
+const array2 = array1;
+console.log(
+  "ref ex of two arrays pointing to the same spot",
+  array1 === array2
+);
+array2.push("Ruben");
+console.log("after push", "array 2", array2, "array 1", array1);
+
+//spread operator ex
+const array3 = [4, 5, 6, { name: "Doreen" }];
+const array4 = [...array3];
+// console.log("spread operator ex", array3, array4);
+// array4.push("Yildiz");
+// console.log("after push", array3, array4);
+array4[3].student = true;
+console.log("after nested ex with spread", array3, array4);
+
+//<==================Deep Copy==================>
+//true deep copy is with JSON.parse and JSON.stringify
+const array5 = [1, 2, 3, [4, 5, 6], { name: "Ian" }];
+const deepCopyArray = JSON.parse(JSON.stringify(array5));
+deepCopyArray[4].name = "Zeynep";
+console.log("deep copy ex", array5, deepCopyArray);
+console.log(array5 === deepCopyArray);
+
+//<===========setinterval example ===============>
+let counter = 0;
+const intervalId = setInterval(() => {
+  counter++;
+  console.log("this is the interval", counter);
+  if (counter > 5) {
+    clearInterval(intervalId);
+    console.log("hopefully the timer has stopped");
   }
-});
-console.log("sorted Asc", sortedAsc);
-
-//To sort descending, just switch the positive and negative numbers being returned
-const sortedDesc = numbers.sort((a, b) => {
-  if (a > b) {
-    return -5;
-  } else if (a < b) {
-    return 7;
-  } else {
-    return 0;
-  }
-});
-
-//one-liner version of sort
-const sortedOneLine = numbers.sort((a, b) => a - b);
-console.log("sorted dec ", sortedOneLine);
-
-const reversed = sortedOneLine.reverse();
-console.log("super easy to reverse", reversed);
-let ourStr = "this is super fun";
-const revStr = ourStr.split("").reverse();
-console.log("here is our rev str", revStr.join("----"));
-
-//Eiad's question about comparing strings with > or <.
-//It checks the character code of a letter and does the math to check
-const aaa = "Aaa";
-const bbb = "bbb";
-console.log("Eiad question", aaa < bbb);
+}, 1000);
